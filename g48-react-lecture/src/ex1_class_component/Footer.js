4515f4cb0b9d621649b304fc02e74168944e510a
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-04-03 10:44:47 +0200

    React Fundamentals - Added Bootstrap@5 + created Class Components

---
+++ g48-react-lecture/src/ex1_class_component/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+        <footer className="bg-danger text-white text-center mt-5 py-5"> &copy; Lexicon 2023 </footer>
+    )
+  }
+}
--- g48-react-lecture/src/index.js
+++ g48-react-lecture/src/index.js
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css"
+
+import App from "./ex1_class_component/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <h1>Hi Group 48</h1>
+    <App/>
 )

4d9204c7620fd487d87f7c058d2a51b9f1e441d7
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-04-03 11:24:22 +0200

    Imported JS for bootstrap

--- g48-react-lecture/src/index.js
+++ g48-react-lecture/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 import App from "./ex2_functions/App";
 

7905362511e0d42f1246694592ee11838efbf61d
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-04-11 10:24:28 +0200

    React Marketplace App - Done

---
+++ react-marketplace/instruction/commands.md
@@ -0,0 +1,5 @@
+## Create a initial project.
+npx create-react-app react-marketplace
+
+## Adding Boostrap to project.
+npm install bootstrap@5
---
+++ react-marketplace/src/App.js
@@ -0,0 +1,60 @@
+import AdList from "./AdList";
+import Header, { Nav } from "./Header";
+
+function App() {
+  const pageTitle = "Marketplace";
+  const headerBackground = "danger";
+  const links = ["Login", "Sign Up", " About"];
+  const logo = "logo.jpg";
+
+  const advertisementsData = [
+    {
+      title: "Ad 1",
+      description: "This is the first advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact1@example.com",
+    },
+    {
+      title: "Ad 2",
+      description: "This is the second advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact2@example.com",
+    },
+    {
+      title: "Ad 3",
+      description: "This is the third advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact3@example.com",
+    },
+    {
+      title: "Ad 4",
+      description: "This is the fourth advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact4@example.com",
+    },
+    {
+      title: "Ad 5",
+      description: "This is the fifth advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact5@example.com",
+    },
+    {
+      title: "Ad 6",
+      description: "This is the sixth advertisement.",
+      image: "https://via.placeholder.com/150",
+      contact: "contact6@example.com",
+    },
+    // Add more advertisements as needed
+  ];
+
+  return (
+    <div className="App">
+      <Nav links={links} logo={logo} />
+      <Header text={pageTitle} bgColor={headerBackground} />
+
+      <AdList adList={advertisementsData} />
+    </div>
+  );
+}
+
+export default App;

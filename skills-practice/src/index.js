b81ade909c2399070fd0b91aaa7e75381519c920
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-04-18 09:46:57 +0200

    Packages Installed - Bootstrap + React-hook-Form

--- skills-practice/src/index.js
+++ skills-practice/src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 root.render(
-    <h1>Hello World!</h1>
+    <h1 className="display-1">Hello World!</h1>
 );

5223f7598b46cc470797b5262c044ba67684593d
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-04-23 10:37:43 +0200

    React fundamentals - Axios - Post to API + Change values for button when booked

--- g48-react-lecture/src/ex8_axios/AxiosDemo.js
+++ g48-react-lecture/src/ex8_axios/AxiosDemo.js
@@ -1,59 +1,81 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const AxiosDemo = () => {
+  const baseURL = "http://localhost:8080";
 
-    const baseURL = 'http://localhost:8080';
+  const [bookings, setBookings] = useState([]);
 
-    const [bookings, setBookings] =useState([]);
+  const getBookingsClickHandler = async () => {
+    console.log("Start");
 
+    await axios
+      .get(baseURL + "/api/v1/booking/from/2024-04-23/to/2024-04-24")
+      .then((response) => {
+        console.log("RESPONSE", response);
 
-    const getBookingsClickHandler = async () => {
-        console.log('Start');
+        if (response.status === 200) {
+          console.log("DATA", response.data);
+          setBookings(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+      });
 
-        await axios.get(baseURL + '/api/v1/booking/from/2024-04-23/to/2024-04-24')
-        .then((response) => {
-            console.log('RESPONSE', response);
+    console.log("End");
+  };
 
-            if(response.status === 200){
-                console.log('DATA', response.data);
-                setBookings(response.data);
-            }
-        })
-        .catch((error) =>{
-            console.log('ERROR: ', error)
-        });
+  const bookBookingClickHandler = async (id, email) => {
+    axios
+      .post(baseURL + "/api/v1/booking/book", { id, email })
+      .then((response) => {
+        console.log("RESPONSE: ", response);
+      })
+      .catch((error) => {
+        console.error("ERROR: ", error);
 
-        console.log('End');
-    };
+        if (error.response) {
+          console.log(error.response.data);
+        }
+      });
 
+    getBookingsClickHandler();
+  };
 
-    return (
-        <div className='container mt-5'>
-
-            <div className='row'>
-                <div className='col'>
-                    <button className='btn btn-info' onClick={getBookingsClickHandler}>Get Bookings</button>
-                </div>
-            </div>
-
-
-            <div className='col mt-3'>
-
-                {bookings.map(booking => (
-                    <div key={booking.id} className='card mb-4 col-mb-3'>
-                        <div className='card-body'>
-                            <h5 className='card-title'>{booking.dateTime}</h5>
-                        </div> 
-                    </div>
-                ))}
-
-            </div>
-
-
-            
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-info" onClick={getBookingsClickHandler}>
+            Get Bookings
+          </button>
         </div>
-    );
+      </div>
+
+      <div className="col mt-3">
+        {bookings.map((booking) => (
+          <div key={booking.id} className="card mb-4 col-mb-3">
+            <div className="card-body">
+              <h5 className="card-title">{booking.dateTime}</h5>
+            </div>
+
+            <div className="d-grid card-footer">
+              <button
+                className={`btn btn-${booking.booked ? "danger" : "success"}`}
+                disabled={`${booking.booked ? "disabled" : ""}`}
+                onClick={() =>
+                  bookBookingClickHandler(booking.id, "test.test@lexicon.se")
+                }
+              >
+                {booking.booked ? "Booked" : "Available"}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AxiosDemo;
